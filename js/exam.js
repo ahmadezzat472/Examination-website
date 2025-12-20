fbe1c5fa714d4a555da1563e4c69876a735e7dd8
Ahmed Ezzat <admedo472@gmail.com>
2025-12-20 19:31:17 +0200

    start js

---
+++ js/exam.js
@@ -0,0 +1,7 @@
+let submitExam = document.querySelector("#submit-exam");
+console.log(submitExam);
+
+
+// handle timer display
+let timerDisplay = document.querySelector("#timer-display");
+console.log(timerDisplay);
