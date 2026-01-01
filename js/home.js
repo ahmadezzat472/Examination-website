80957e69e076f0bcbebc226565e50bba490cc085
Ahmed Ezzat <admedo472@gmail.com>
2026-01-01 02:09:05 +0200

    handle old updates

--- js/home.js
+++ js/home.js
@@ -1,37 +1,38 @@
 import { courses } from "./data.js";
-var selectedCategory = document.getElementById("category")
-var allCourses = Object.entries(courses)
-var grid = document.getElementById("coursesGrid")
-var categories = document.querySelectorAll("li")
+var selectedCategory = document.getElementById("category");
+var allCourses = Object.entries(courses);
+var grid = document.getElementById("coursesGrid");
+var categories = document.querySelectorAll("li");
 
-grid.addEventListener("click", function(e){
-  if(e.target.classList.contains("course-btn")){
-    var courseName = e.target.dataset.course
-    var encodedName = encodeURIComponent(courseName)
-    window.location.href = `course-details.html?course=${encodedName}`
+grid.addEventListener("click", function (e) {
+  if (e.target.classList.contains("course-btn")) {
+    var courseName = e.target.dataset.course;
+    var encodedName = encodeURIComponent(courseName);
+    window.location.href = `course-details.html?course=${encodedName}`;
   }
-})
+});
 
-categories.forEach(function(category){
-  category.addEventListener("click", function(){
-    categories.forEach(function(category){
-      category.classList.remove("bg-surface")
-      category.classList.remove("text-primary-400")
-      category.classList.remove("font-bold")
-      category.classList.remove("dark:text-primary-400")
-    })
-    category.classList.add("bg-surface")
-    category.classList.add("text-primary-400")
-    category.classList.add("font-bold")
-    category.classList.add("dark:text-primary-400")
-    selectedCategory.textContent = category.textContent
-    grid.innerHTML = ""
-    if(category.textContent === "All Courses"){
-      allCourses.forEach(function(course){
-      var totalQuestions = course[1].easy.length
-      var card = document.createElement("div")
-      card.className = "shadow-card rounded-xl w-64 p-5 bg-surface dark:bg-surface-dark flex flex-col gap-4"
-      card.innerHTML = `
+categories.forEach(function (category) {
+  category.addEventListener("click", function () {
+    categories.forEach(function (category) {
+      category.classList.remove("bg-surface");
+      category.classList.remove("text-primary-400");
+      category.classList.remove("font-bold");
+      category.classList.remove("dark:text-primary-400");
+    });
+    category.classList.add("bg-surface");
+    category.classList.add("text-primary-400");
+    category.classList.add("font-bold");
+    category.classList.add("dark:text-primary-400");
+    selectedCategory.textContent = category.textContent;
+    grid.innerHTML = "";
+    if (category.textContent === "All Courses") {
+      allCourses.forEach(function (course) {
+        var totalQuestions = course[1].easy.length;
+        var card = document.createElement("div");
+        card.className =
+          "shadow-card rounded-xl w-64 p-5 bg-surface dark:bg-surface-dark flex flex-col gap-4";
+        card.innerHTML = `
           <div class="w-12 bg-gradient-to-r from-blue-400 to-blue-800 rounded-lg p-2 shadow-md shadow-blue-300">
           <img src="${course[1].imageSrc}" alt="${course[1].name}">
         </div>
@@ -64,18 +65,18 @@ categories.forEach(function(category){
             </button>
           </div>
         </div>
-      `
-      grid.appendChild(card)
-      })
-    }
-    else{
-      var showedCourses = allCourses.filter(function(course){
-        return course[1].category === category.textContent
-      })
-      showedCourses.forEach(function(course){
-        var totalQuestions = course[1].easy.length
-        var card = document.createElement("div")
-        card.className = "shadow-card rounded-xl w-64 p-5 bg-surface dark:bg-surface-dark flex flex-col gap-4"
+      `;
+        grid.appendChild(card);
+      });
+    } else {
+      var showedCourses = allCourses.filter(function (course) {
+        return course[1].category === category.textContent;
+      });
+      showedCourses.forEach(function (course) {
+        var totalQuestions = course[1].easy.length;
+        var card = document.createElement("div");
+        card.className =
+          "shadow-card rounded-xl w-64 p-5 bg-surface dark:bg-surface-dark flex flex-col gap-4";
         card.innerHTML = `
             <div class="w-12 bg-gradient-to-r from-blue-400 to-blue-800 rounded-lg p-2 shadow-md shadow-blue-300">
             <img src="${course[1].imageSrc}" alt="${course[1].name}">
@@ -109,19 +110,20 @@ categories.forEach(function(category){
               </button>
             </div>
           </div>
-        `
-        grid.appendChild(card)
-      })
+        `;
+        grid.appendChild(card);
+      });
     }
-  })
-})
+  });
+});
 
 document.addEventListener("DOMContentLoaded", function () {
-  selectedCategory.textContent = "All Courses"
+  selectedCategory.textContent = "All Courses";
   allCourses.forEach(function (course) {
-    var totalQuestions = course[1].easy.length
-    var card = document.createElement("div")
-    card.className = "shadow-card rounded-xl w-64 p-5 bg-surface dark:bg-surface-dark flex flex-col gap-4"
+    var totalQuestions = course[1].easy.length;
+    var card = document.createElement("div");
+    card.className =
+      "shadow-card rounded-xl w-64 p-5 bg-surface dark:bg-surface-dark flex flex-col gap-4";
     card.innerHTML = `
             <div class="w-12 bg-gradient-to-r from-blue-400 to-blue-800 rounded-lg p-2 shadow-md shadow-blue-300">
         <img src="${course[1].imageSrc}" alt="${course[1].name}">
@@ -155,7 +157,7 @@ document.addEventListener("DOMContentLoaded", function () {
           </button>
         </div>
       </div>
-    `
-    grid.appendChild(card)
-  })
-})
+    `;
+    grid.appendChild(card);
+  });
+});
